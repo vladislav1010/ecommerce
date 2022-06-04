92da06b629fcd49835e2a1f13e4294599ef4105b
Владислав Савченко <vs101ff@gmail.com>
2022-06-04 08:34:11 +0300

    feat(github-theme): init

---
+++ packages/github-theme/index.ts
@@ -0,0 +1,8 @@
+import { extendTheme } from "@chakra-ui/react";
+import { mode } from "@chakra-ui/theme-tools";
+
+type Dict = Record<string, any>;
+
+const theme = extendTheme({});
+
+export default theme;
